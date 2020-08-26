60bfbbea57b7cac7df037c939f1a334cbc2801e3
carrieah14 <69916469+carrieah14@users.noreply.github.com>
2020-08-26 16:27:33 -0400

    Add Hello World
    Signed-off-by: nghiatle20 <69871931+nghiatle20@users.noreply.github.com>

---
+++ VSHelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VSHelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is SSE 662");
+		}
+	}
+}
